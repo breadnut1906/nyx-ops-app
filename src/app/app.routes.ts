7b4649e8b4d37aaf59eefb405cf03dd1ea8974b7
breadnut1906 <hmsbreadnut1906@gmail.com>
2025-01-08 14:50:11 +0800

    new updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./views/layout/layout.component').then((m) => m.LayoutComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
+                title: 'Login',
+            },
+            {
+                path: 'home',
+                loadComponent: () => import('./views/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+                title: 'Operations Manager | Dashboard',
+            }
+        ]
+    }
+];
